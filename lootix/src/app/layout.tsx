ab8f58799e0595dd786e7b0f1331dab0ac879dd5
bmack08 <bmccoy67@gmail.com>
2025-04-27 15:51:17 -0400

    Fix layout hydration mismatch by cleaning body styles

--- lootix/src/app/layout.tsx
+++ lootix/src/app/layout.tsx
@@ -9,9 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-zinc-950 text-white isolate">
-      <Header />
-      {children}
+      <body className="bg-zinc-950 text-white antialiased">
+        <Header />
+        {children}
       </body>
     </html>
   );
