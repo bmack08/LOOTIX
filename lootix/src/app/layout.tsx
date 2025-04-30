aec3f9f0e53ce89ae82fd870af049f9fe0b9d5ee
bmack08 <bmccoy67@gmail.com>
2025-04-30 17:56:18 -0400

    Fixed Footer TSX file

--- lootix/src/app/layout.tsx
+++ lootix/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import '../styles/globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ['latin'] });
 
